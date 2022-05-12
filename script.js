9fab74e0754785bccbb5a8abd7cf3efe027bf32b
muharib0410 <abagithub0410@gmail.com>
2022-05-12 22:47:51 +0500

    commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const colors = document.querySelectorAll('.color-box h2');
 function generateColors() {
     colors.forEach ((color)=> {
         let hexCode = '#' + Math.random().toString(16).substring(2, 8);
-        color.getElementsByClassName.backgroundColor = hexCode;
+        color.style.backgroundColor = hexCode;
         color.innerHTML = hexCode;
     });
 }
